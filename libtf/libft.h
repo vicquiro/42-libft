29886b082e2fc516b81fa32dcc39e691c67e44ac
Victor Quiroga Reyes <vquiroga@c3r6s6.42madrid.com>
2022-09-16 15:36:05 +0200

    strchr

--- libtf/libft.h
+++ libtf/libft.h
@@ -6,7 +6,7 @@
 /*   By: vquiroga <vquiroga@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 19:06:12 by vquiroga          #+#    #+#             */
-/*   Updated: 2022/09/16 13:42:20 by vquiroga         ###   ########.fr       */
+/*   Updated: 2022/09/16 15:33:44 by vquiroga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t			ft_strlen(const char *s);
 void			*ft_memset(void *b, int c, size_t len);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
+//void			*memmove(void *dst, const void *src, size_t len);
 void			ft_bzero(void *s, size_t n);
+char			*ft_strchr(const char *s, int c);
+//char			*ft_strrchr(const char *s, int c);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
